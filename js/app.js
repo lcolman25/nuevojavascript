7e07b4ef6be479ed70d07a8f47a86f19a6c7a130
lucas colman <lcolman2525@gmail.com>
2022-06-19 17:24:38 -0300

    se arregla proligidad de codigo

--- js/app.js
+++ js/app.js
@@ -11,24 +11,26 @@ const suma = (a, b) => {
      let resultado = a + b
      alert('usted debe pagar '+ resultado + ' pesos')
 }
+let Bienvenido ='Bienvenido a marihiguana';
 
-let Bienvenido = alert ('Bienvenido a marihiguana');
+
+ alert(Bienvenido)
 
 let nombreDeUsuario = prompt('Nombre');
      while( nombreDeUsuario ===  ''|| !isNaN(nombreDeUsuario)){
-     nombreDeUsuario = prompt('Ingrese nombre sin espacios ni números')
+     nombreDeUsuario = prompt('Ingrese nombre sin espacios ni números');
 }
 let apellidoDeUsuario = prompt('apellido');
      while( apellidoDeUsuario === ''|| !isNaN(apellidoDeUsuario)){
-          apellidoDeUsuario = prompt ('ingrese su apellido sin espacio ni números ') 
+          apellidoDeUsuario = prompt ('ingrese su apellido sin espacio ni números ') ;
      }
 let usuario = nombreDeUsuario + " " + apellidoDeUsuario;
-     alert('Bienvenido' + " " + usuario)
+    
      for (let index = 0; index < apellidoDeUsuario.length; index++) {
           let caracter = apellidoDeUsuario[index];
-          alert('Deletraremos tu apellido , si no lo escribiste bien carga nuevamente la pagina ' + caracter)
+          alert('Deletraremos tu apellido , si no lo escribiste bien carga nuevamente la pagina ' + caracter);
      }
-
+     alert('Bienvenido' + " " + usuario)
 const selectorSemillas = () => {
      producto = prompt('Aquí te dejamos las semillas que tenemos en stock elije una de ellas y se le brindará el precio: despink , sour destro , cherryog , mohanthai , og 342 ,river haze , amnecia , lemon blue , ultra mahona')
      let precio;
@@ -63,50 +65,48 @@ const selectorSemillas = () => {
 }
 
 const cuantasSemillas = () => {
-let cantidad =  prompt('¿Cuantos packs vas a llevar?');
-while (isNaN(parseInt(cantidad)) || cantidad >= 11 ){
- cantidad = prompt('Ingrese solo números de packs ,menor a 10 que es la cantidad habilitada ')
-}
-if(cantidad == 1 ){
-     alert('¿Seguro desea comprar solo uno?')   
-}
-else if(cantidad == 5 ){
-     alert('Con un packs más se le hará un 20% off que se deberá avisar en el momento de retirar el producto')
-}
-else if (cantidad == 10 ){
-     alert('Felicidades con la compra de 10 packs, te llevas 1 de REGALO ')   
-}
-else{
-     alert('Gracias')
-}
- return cantidad;
+     let cantidad =  prompt('¿Cuantos packs vas a llevar?');
+     while (isNaN(parseInt(cantidad)) || cantidad >= 11 ){
+     cantidad = prompt('Ingrese solo números de packs ,menor a 10 que es la cantidad habilitada ');
+     }
+     if(cantidad == 1 ){
+          alert('¿Seguro desea comprar solo uno?');   
+     }
+     else if(cantidad == 5 ){
+          alert('Con un packs más se le hará un 20% off que se deberá avisar en el momento de retirar el producto');
+     }
+     else if (cantidad == 10 ){
+          alert('Felicidades con la compra de 10 packs, te llevas 1 de REGALO '); 
+     }
+     else{
+          alert('Gracias');
+     }
+     return cantidad;
 }
 
  multiplicacion (selectorSemillas(),cuantasSemillas());
-     class Ventas {
-          constructor (nombre, precio){
-            this.nombre = nombre
-            this.precio = precio
-          } 
+ class Ventas {
+     constructor (nombre, precio){
+      this.nombre = nombre;
+      this.precio = precio;
      } 
-     const ventas1 = new Ventas( "despink", 1410)
-     const ventas2 = new Ventas("sour destro", 1600)
-     const ventas3 = new Ventas( "cherryog", 1590)
-     const ventas4 = new Ventas( "mohanthai", 2000)
-     const ventas5 = new Ventas( "og 342", 1420)
-     const ventas6 = new Ventas( "river haze", 1850)
-     const ventas7 = new Ventas( "amnecia", 1600)
+ } 
+     const ventas1 = new Ventas( "despink", 1410);
+     const ventas2 = new Ventas("sour destro", 1600);
+     const ventas3 = new Ventas( "cherryog", 1590);
+     const ventas4 = new Ventas( "mohanthai", 2000);
+     const ventas5 = new Ventas( "og 342", 1420);
+     const ventas6 = new Ventas( "river haze", 1850);
+     const ventas7 = new Ventas( "amnecia", 1600);
 
-
- 
  class DetalleVentas {
      constructor( producto,cantidad){
-          this.producto = producto;
-          this.cantidad = cantidad;
+      this.producto = producto;
+      this.cantidad = cantidad;
      }
     totalDeCompra() {
-          let subtotal = this.producto.precio * this.cantidad;
-          return subtotal;
+      let subtotal = this.producto.precio * this.cantidad;
+      return subtotal;
      }   
 }
      const detalleVentas1 = new DetalleVentas(ventas1, 3);
@@ -117,38 +117,68 @@ else{
      const detalleVentas6 = new DetalleVentas(ventas6, 1);
      const detalleVentas7 = new DetalleVentas(ventas7, 4);
 
-     console.log(detalleVentas1.totalDeCompra())
-     console.log(detalleVentas2.totalDeCompra())
-     console.log(detalleVentas3.totalDeCompra())
-     console.log(detalleVentas4.totalDeCompra())
-     console.log(detalleVentas5.totalDeCompra())
-     console.log(detalleVentas6.totalDeCompra())
-     console.log(detalleVentas7.totalDeCompra())
+     console.log(detalleVentas1.totalDeCompra());
+     console.log(detalleVentas2.totalDeCompra());
+     console.log(detalleVentas3.totalDeCompra());
+     console.log(detalleVentas4.totalDeCompra());
+     console.log(detalleVentas5.totalDeCompra());
+     console.log(detalleVentas6.totalDeCompra());
+     console.log(detalleVentas7.totalDeCompra());
 
-     class PedidoEcho{
-          constructor(fecha,detalles){
+class PedidoEcho{
+     constructor(fecha,detalles){
           this.fecha = fecha;
           this.detalles = detalles;
      }
      calcularPedidoEcho(){
           let total=0;
-           for(const dp of this.detalles){
-           total= total + dp.totalDeCompra();
+               for(const dp of this.detalles){
+               total= total + dp.totalDeCompra();
           }
       return total;
      }
 }
 const array = [ ];
-     array.push(detalleVentas1)
-     array.push(detalleVentas2)
-     array.push(detalleVentas3)
-     array.push(detalleVentas4)
-     array.push(detalleVentas5)
-     array.push(detalleVentas6)
-     array.push(detalleVentas7)
+     array.push(detalleVentas1);
+     array.push(detalleVentas2);
+     array.push(detalleVentas3);
+     array.push(detalleVentas4);
+     array.push(detalleVentas5);
+     array.push(detalleVentas6);
+     array.push(detalleVentas7);
 
 
-const PedidoEcho1= new PedidoEcho(new Date(),array)
+const PedidoEcho1= new PedidoEcho(new Date(),array);
 
-console.log (PedidoEcho1.calcularPedidoEcho())
- 
+console.log (PedidoEcho1.calcularPedidoEcho());
+
+/*let carrito =[]
+
+
+
+let gardadorhtml =document.getElementsByClassName('principal')
+let showAllProducts= document.getElementById('showAllProducts')
+
+function mostrarPeoductos(){
+    productos.forEach((product) =>{
+        let caja = document.createElement('div')
+           caja.setAttribute( "class","desmo")
+            showAllProducts.append(caja)
+
+        let img = document.createElement("img")
+            img.setAttribute("src",product.img)
+
+        let nombre = document.createElement("h3")
+            nombre.innerText = (product.nombre) 
+
+        let precio = document.createElement('p')
+            precio.innerText = (product.precio) 
+
+        let botonAlCarrito=document.createElement("button")
+            botonAlCarrito.innerText=("agregar al carrito")
+
+          caja.append(img,nombre,precio,botonAlCarrito)
+    })
+}
+
+mostrarPeoductos()*/
---
+++ js/productos.js
@@ -0,0 +1,75 @@
+const productos =[
+    
+    { id:"1",
+    categoria:'semillas',
+    nombre :'DESPINK (packs x5 semillas)',
+    precio:1410,
+    img:"https://silverriverseeds.com/wp-content/uploads/2019/02/Despink-10-WEB-600x562.jpg"
+    },
+    {
+        id:"2",
+        categoria:'semillas',
+        nombre:'APPLE COOKIES (pac x5 semillas)',
+        precio:1550,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/apple-cookies-1-WEB-600x548.jpg"
+    },
+    {
+        id:'3',
+        categoria:'semillas',
+        nombre:'SOUR DESTRO (pac x5 semillas)',
+        precio:1600,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/sour-destro-6-WEB.jpg"
+    },
+    {
+        id:'4',
+        categoria:'semillas',
+        nombre:'CHERRY OG (pac x5 semillas)',
+        precio:1590,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/cherry-og-2-PORTADA-WEB-590x786.jpg",
+    },
+    {
+        id:'5',
+        categoria:'semillas',
+        nombre:'MOHAN THAI (pac x5 semillas)',
+        precio:2000,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/mohan-thai-4-WEB.jpg",
+    },
+    {
+        id:'6',
+        categoria:'semillas',
+        nombre:'OG 324 (pac x5 semillas)',
+        precio:1420,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/OG-324-Portada-WEB.jpg",
+
+    },
+    {
+        id:'7',
+        categoria:'semillas',
+        nombre:'RIVER HAZE (pac x5 semillas)',
+        precio:1850,
+        img:"https://silverriverseeds.com/wp-content/uploads/2019/03/river-haze-9-Portada-WEB.jpg",
+
+    },
+    {
+        id:"8",
+        categoria:'semillas',
+        nombre:'Amnesia (pac x5 semillas)',
+        precio:1600,
+        img:"https://growshopdelpaso.com/wp-content/uploads/2021/08/AUTO-AMNESIA-AMAZON-2.jpg",
+    },
+    {
+        id:"9",
+        categoria:'semillas',
+        nombre:'Lemon blue cheese auto (pac x5 semillas)',
+        precio:1400,
+        img:"https://growshopdelpaso.com/wp-content/uploads/2021/04/blue-lemon.jpg",
+    },
+    {
+        id:"10",
+        categoria:'semillas',
+        nombre:'Ultra Mahan Ram Auto (pac x5 semillas)',
+        precio:1900,
+        img:"https://growshopdelpaso.com/wp-content/uploads/2021/04/ultra-mohan-TARJETA-w-590x786-1.jpg",
+    },
+
+]
