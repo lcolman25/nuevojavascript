fca14c32f815682dcc7246c5c8c7419dcfe37901
lucas colman <lcolman2525@gmail.com>
2022-06-23 16:03:20 -0300

    se agrega evento

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ let carrito =[]
 
 let gardadorhtml =document.getElementsByClassName('principal')
 let showAllProducts= document.getElementById('showAllProducts')
+let div= document.getElementById('div')
 
 function mostrarPeoductos(){
     productos.forEach((product) =>{
@@ -57,7 +58,114 @@ function mostrarPeoductos(){
             botonAlCarrito.innerText=("agregar al carrito")
 
           caja.append(img,nombre,precio,botonAlCarrito)
+           
+          botonAlCarrito.addEventListener('click',function(){
+               carrito.push(product)
+               alert("Agregaste " + product.nombre + " al carrito")
+               div.innerHTML=``
+               showcart()
+          })
     })
 }
 
 mostrarPeoductos()
+
+//carrito//
+let showcartproducts =document.getElementById("showcartproductscarrito")
+let cartlista = document.getElementById("cartlista")
+let mostrarcarrito =document.getElementById("mostrarCarrito")
+ 
+let alertCarrito= document.createElement("h2")
+alertCarrito.setAttribute("class","muestraVacia")
+
+ if(!carrito.length){
+     alertCarrito.innerText=("El carrito se encuentra sin productos")
+     div.append(alertCarrito)
+}
+
+function showcart(){
+     alertCarrito.remove()
+
+     carrito.forEach((element) =>{
+          const divCart=document.createElement("div")
+          divCart.setAttribute("class","vistaaCompra")
+          divCart.innerHTML=`
+          <img src="${element.img}">
+          <h3>${element.nombre}<h3>
+          <h3>${element.precio}<h3>
+          <button class= "eliminar"data-id=${element.id}>X</button>`
+
+           div.appendChild(divCart)
+
+     })
+
+     let eliminarcompra=document.querySelector(".eliminar")
+
+
+ const total1 = carrito.map((element)=> (element.precio)).reduce((carritoTotalPrecio,
+     currenitemPrecio)=> carritoTotalPrecio + currenitemPrecio, 0);
+   
+
+ let totalDeCompra=document.createElement("h4")
+     totalDeCompra.innerHTML=("total:" + "$" + total1 )
+     div.append(totalDeCompra)
+
+ let vaciarCarrito =document.createElement("button")
+  vaciarCarrito.innerHTML=("desechar todo")
+  div.append(vaciarCarrito)
+   
+  vaciarCarrito.onclick=()=>{
+     carrito=[]
+     div.innerHTML=``
+     console.log(carrito)
+  }
+
+}
+
+//buscador//
+let buscador =document.getElementById("inputsearch")
+let filtrador = document.getElementById("filtro")
+
+function filtrarproductos(){
+    let verTodo =document.createElement("button") 
+    verTodo.setAttribute("class","verTodo")
+    verTodo.innerHTML =("Ver Todo")
+    showAllProducts.append(verTodo)
+    const filterproduct = productos.filter((product)=>product.categoria === buscador.value)
+  
+
+    filterproduct.forEach((filter)=>{
+          let resDeFiltro=document.createElement("div")
+           resDeFiltro.setAttribute("class","cajaBusquedas")
+          showAllProducts.append(resDeFiltro)
+
+          let imgBusqueda=document.createElement("img")
+          imgBusqueda.setAttribute("src" ,filter.img)
+
+          let nombreBusqueda=document.createElement("h3")
+          nombreBusqueda.innerText=(filter.nombre)
+
+          let precioBusqueda=document.createElement("p")
+          precioBusqueda.innerText=(filter.precio)
+
+          let buyButtonBusqueda=document.createElement("button")
+          buyButtonBusqueda.innerText = ("agregar al carrito")
+
+          resDeFiltro.append(imgBusqueda, nombreBusqueda, precioBusqueda, buyButtonBusqueda)
+     })
+     
+
+    verTodo.onclick = () =>{
+          showAllProducts.innerHTML=``
+          mostrarPeoductos()
+     }
+}
+buscador.onchange = () => {
+     showAllProducts.innerHTML=``
+     filtrarproductos()
+}
+
+filtrador.onclick = () => {
+     showAllProducts.innerHTML=``
+     filtrarproductos()
+}
--- js/productos.js
+++ js/productos.js
@@ -71,5 +71,87 @@ const productos =[
         precio:1900,
         img:"https://growshopdelpaso.com/wp-content/uploads/2021/04/ultra-mohan-TARJETA-w-590x786-1.jpg",
     },
+    {
+        id:"11",
+        categoria:'hojillas',
+        nombre:'Hojilla Blunt',
+        precio:150,
+        img:"https://http2.mlstatic.com/D_NQ_NP_724484-MLU47125284177_082021-O.webp",
+    },
+    {
+        id:"12",
+        categoria:'hojillas',
+        nombre:'Hojilla Hornet',
+        precio:120,
+        img:"https://http2.mlstatic.com/D_NQ_NP_600207-MLU47208182764_082021-O.webp",
+    },
+    {
+        id:"13",
+        categoria:'hojillas',
+        nombre:'Hojilla Lion Rolling Circus Celulosa Big Smoke King Size',
+        precio:120,
+        img:"https://http2.mlstatic.com/D_NQ_NP_700896-MLU50332603468_062022-O.webp",
+    },
+    {
+        id:"14",
+        categoria:'hojillas',
+        nombre:'Hojilla Bulldog Red Regular Dw',
+        precio:100,
+        img:"https://http2.mlstatic.com/D_NQ_NP_793629-MLU50059458345_052022-O.webp",
+    },
+    {
+        id:"15",
+        categoria:'hojillas',
+        nombre:'Hojilla Tamaño Chico Moon',
+        precio:50,
+        img:"https://http2.mlstatic.com/D_NQ_NP_795286-MLU49988758692_052022-O.webp",
+    },
+    {
+        id:"16",
+        categoria:'desmorrugador',
+        nombre:'Desmorrugador Aluminio De 4 Partes',
+        precio:920,
+        img:"https://http2.mlstatic.com/D_NQ_NP_956590-MLU47018622029_082021-O.webp",
+    },
+    {
+        id:"17",
+        categoria:'desmorrugador',
+        nombre:'Desmorrugador Darth Vader - Excelente Calidad',
+        precio:720,
+        img:"https://http2.mlstatic.com/D_NQ_NP_620758-MLU46658247849_072021-O.webp",
+    },
+    {
+        id:"18",
+        categoria:'desmorrugador',
+        nombre:'Desmorrugador Metal Imantado Con Depósito Tambor Multicolor',
+        precio:838,
+        img:"https://http2.mlstatic.com/D_NQ_NP_933524-MLA49205629584_022022-O.webp",
+    },
+    {
+        id:"19",
+        categoria:'desmorrugador',
+        nombre:'Desmorrugador De Metal Hamburguesa',
+        precio:750,
+        img:"https://http2.mlstatic.com/D_NQ_NP_730819-MLU47018465071_082021-O.webp",
+    },
+    {
+        id:"20",
+        categoria:'desmorrugador',
+        nombre:'Desmorrugador Esfera Del Dragón 4 Estrellas - Dragon Ball Z',
+        precio:600,
+        img:"https://http2.mlstatic.com/D_NQ_NP_915438-MLU49613683702_042022-O.webp",
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
 
 ]
