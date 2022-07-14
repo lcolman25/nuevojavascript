a9cfd29e700fee80040356b96fb58413fd1d51cb
lucas colman <lcolman2525@gmail.com>
2022-07-14 14:28:12 -0300

    json

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ if(JSON.parse(localStorage.getItem("carrito"))){
     showcart();
     
 }else{localStorage.setItem("carrito",JSON.stringify([]))
-carrito=Json.parse(localStorage.getItem("carrito"))
+carrito=JSON.parse(localStorage.getItem("carrito"))
 
 }
 
